65685bd50dddc77608db4df719525774c18ed315
Ars Vladimirov <grokhael@gmail.com>
2023-08-26 01:23:06 +0600

    Replace lines in target_file

--- src/main.py
+++ src/main.py
@@ -65,6 +65,7 @@ if __name__ == "__main__":
     force_stats = forces.get("force_stats")
     force_visuals = forces.get("force_visuals")
     force_demo = forces.get("force_demo")
+    force_demo = forces.get("force_demo")
     force_texts = forces.get("force_texts")
 
     settings["force_texts"] = force_texts
@@ -72,6 +73,7 @@ if __name__ == "__main__":
     project_repo.build_stats(force=force_stats, settings=stat_options)
     project_repo.build_visualizations(force=force_visuals, settings=vis_options)
 
+    project_repo.build_demo(force=force_demo)
     project_repo.build_demo(force=force_demo)
     project_repo.build_texts(force=force_texts, preview_class=o.PREVIEW_CLASS)
 
